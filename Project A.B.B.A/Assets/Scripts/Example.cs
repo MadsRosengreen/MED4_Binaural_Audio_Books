4abe20770cab776ff8918d125fc47eb9d78dc5a2
VictorBuch <47211196+VictorBuch@users.noreply.github.com>
2020-03-16 16:34:10 +0100

    Added mincrophone playback and recording

---
+++ Project A.B.B.A/Assets/Scripts/Example.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    // Start recording with built-in Microphone and play the recorded audio right away
+    void Start()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Microphone.Start(null, true, 10, 44100);
+        while (!(Microphone.GetPosition(null) > 0)) { }
+        audioSource.Play();
+    }
+}
