36208ee06dc5858c9fde2b73a07ddc77f68be5da
martinkarlik <47499520+martinkarlik@users.noreply.github.com>
2020-04-08 16:21:01 +0200

    preparing ap for gui

--- Project A.B.B.A 2.0/simulating_gyroscope.py
+++ Project A.B.B.A 2.0/simulating_gyroscope.py
@@ -1,7 +1,9 @@
+import pyglet
 import sofa
 import matplotlib.pyplot as plt
 import numpy as np
 import sounddevice as sd
+import soundfile as sf
 from scipy.signal import *
 
 hrtf_database = sofa.Database.open('QU_KEMAR_anechoic_3m.sofa')
@@ -10,43 +12,47 @@ hrtf_database = sofa.Database.open('QU_KEMAR_anechoic_3m.sofa')
 sampling_freq = 48000
 sd.default.samplerate = sampling_freq
 sd.default.channels = (1, 2)
-rec_time = 20 # seconds
+rec_time = 1 # seconds
 
 mic_data = sd.rec(rec_time * sampling_freq)
 mic_data_transposed = mic_data.transpose()
+
 print("Recording...")
 sd.wait()
 print("Recording ended.")
 
-# time_vector = np.arange(rec_time * sampling_freq) / sampling_freq
-# sinusoid = np.reshape(0.05*np.sin(2*np.pi*600*time_vector), (-1, 1))
-# sinusoid_transposed = sinusoid.transpose()
 
-# sound convolution
+population = [(90, 200), (91, 20), (92, 20), (93, 20), (94, 20), (95, 20), (96, 20), (97, 20), (98, 20),
+              (99, 20), (100, 20), (101, 20), (-1, 200)]
 
 output_ear1 = np.zeros([1, rec_time * sampling_freq])
 output_ear2 = np.zeros([1, rec_time * sampling_freq])
-filter_state1 = 0
-filter_state2 = 0
 
-number_of_positions = 90
-total_samples = sampling_freq * rec_time
 
-for i in np.arange(number_of_positions):
-    ir_ear1 = hrtf_database.Data.IR.get_values(indices={"M": i, "R": 0, "E": 0})
-    ir_ear2 = hrtf_database.Data.IR.get_values(indices={"M": i, "R": 1, "E": 0})
 
-    start_index = int(i * total_samples / number_of_positions)
-    end_index = int((i + 1) * total_samples / number_of_positions)
 
-    if i == 0:
+
+elapsed_duration = 0
+
+for position in population:
+    angle = position[0]
+    duration = position[1]
+
+    ir_ear1 = hrtf_database.Data.IR.get_values(indices={"M": position[0], "R": 0, "E": 0})
+    ir_ear2 = hrtf_database.Data.IR.get_values(indices={"M": position[0], "R": 1, "E": 0})
+
+    start_index = elapsed_duration
+    end_index = elapsed_duration + duration
+    elapsed_duration += duration
+
+    if start_index == 0:
         initial_state1 = lfilter_zi(ir_ear1, 1)
         initial_state2 = lfilter_zi(ir_ear2, 1)
 
         output_ear1[0, start_index:end_index], filter_state1 = \
             lfilter(ir_ear1, 1, mic_data_transposed[0, start_index:end_index], zi=initial_state1)
         output_ear2[0, start_index:end_index], filter_state2 = \
-            lfilter(ir_ear2, 1,mic_data_transposed[0, start_index:end_index], zi=initial_state2)
+            lfilter(ir_ear2, 1, mic_data_transposed[0, start_index:end_index], zi=initial_state2)
 
     else:
         output_ear1[0, start_index:end_index], filter_state1 = \
@@ -54,24 +60,9 @@ for i in np.arange(number_of_positions):
         output_ear2[0, start_index:end_index], filter_state2 = \
             lfilter(ir_ear2, 1, mic_data_transposed[0, start_index:end_index], zi=filter_state2)
 
-# playback
-
-output = np.append(output_ear1.transpose(), output_ear2.transpose(), axis=1)
-sd.play(output)
 
 
 print("Playing...")
 sd.wait()
 print("Playback ended.")
-
-# plotting
-
-time_vector = np.arange(output.shape[0])
-plt.figure(figsize=(15, 5))
-plt.title("Output")
-legend = ["Ear1", "Ear 2"]
-for i in range(2):
-    plt.plot(time_vector, output[:, i])
-plt.legend(legend)
-plt.show()
 
