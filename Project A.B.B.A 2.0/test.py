f21c111b228d6dfad7890ee2ea6e6e2e19b96731
martinkarlik <47499520+martinkarlik@users.noreply.github.com>
2020-04-08 11:11:20 +0200

    created python env

---
+++ Project A.B.B.A 2.0/test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+print(np.arange(3))
+
+print("changes")
---
+++ Project A.B.B.A/Assets/Scripts/SaveAudio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using System.Collections.Generic;
+public class SaveAudio : MonoBehaviour
+{
+
+    AudioClip myAudioClip;
+    public static int i;
+
+    void Start() { i = 0; }
+    void Update() { }
+    void OnGUI()
+    {
+        //int i = 0;
+        if (GUI.Button(new Rect(10, 10, 60, 50), "Record"))
+        {
+            myAudioClip = Microphone.Start(null, false, 10, 44100);
+        }
+
+        if (GUI.Button(new Rect(10, 70, 60, 50), "Save"))
+        {
+            //Debug.Log(Path.Combine(Application.dataPath, "myfile" + i + ".wav"));
+            //Debug.Log(System.IO.File.Exists(Path.Combine(Application.dataPath, "myfile" + i + ".wav")));
+            if (System.IO.File.Exists(Path.Combine(Application.dataPath, "myfile" + i +".wav")))
+            {
+                i += 1;
+                SavWav.Save("myfile" + i, myAudioClip);
+            }
+            else
+            {
+                SavWav.Save("myfile" + i, myAudioClip);
+                //audio.Play();
+            }
+        }
+        if (GUI.Button(new Rect(10, 130, 60, 50), "Delete"))
+        {
+            if(i -1 > 0)
+            {
+                DeleteAudio(Path.Combine(Application.dataPath, "myfile" + i + ".wav"));
+                i--;
+            } else { i = 0; }
+            
+        }
+    }
+
+    void DeleteAudio(String filename)
+    {
+        System.IO.File.Delete(filename);
+    }
+}
